bdcdc6885a7397bad4bdcbfec5677642837a399f
John Petersson <johnpetersson@gmail.com>
2017-04-20 03:22:51 +0200

    Concatenated version of sketch js files. File structure refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,46 @@
-var gulp = require("gulp");
-var livereload = require('gulp-livereload');
+var gulp = require("gulp"),
+livereload = require('gulp-livereload'),
+concat = require("gulp-concat");
 
 gulp.task('default', ['watch']);
 
 gulp.task('watch', function() {
 	livereload.listen();
 	gulp.watch('**/*.css', ['reload']);
-	gulp.watch('**/*.js', ['reload']);
+	gulp.watch('**/*.js', ['concat', 'reload']);
 	gulp.watch('**/*.html', ['reload']);
 });
 
 gulp.task('reload', function() {
 	livereload.reload();
 }); 
+
+var sketchFolder = "sketch/";
+var sketchFiles = [
+'constants.js',
+'helpers/fifoqueue.js',
+'input/inputenum.js',
+'gameentities/gameboard.js',
+'respawnpopup.js',
+'followcam.js',
+'input/joystick.js',
+'input/inputmodule.js',
+'input/manualinput.js',
+'input/perlininput.js',
+'gameentities/player.js',
+'gameentities/particle.js',
+'gameentities/blob.js',
+'blobs.js',
+'gameentities/blobmanager.js'
+];
+
+
+sketchFiles.forEach(function(fileName, index) {
+	sketchFiles[index] = sketchFolder + fileName;
+});
+
+gulp.task('concat', function() {
+	gulp.src(sketchFiles)
+	.pipe(concat('concat.js'))
+	.pipe(gulp.dest('./concat/'));
+});
